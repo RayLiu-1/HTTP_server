f4818b1a35d7af5f390355fdccf2be196ca15ecd
RayLiu-1 <ruli7792@colorado.edu>
2016-10-19 17:33:08 -0600

    test

--- Server.c
+++ Server.c
@@ -109,10 +109,10 @@ void *connection_handler(void *sockfd) {
 			pch = strtok(NULL, " ");
 			if (strlen(pch) == 0 || pch[0] == '/') {
 				strcpy(buf, "HTTP/1.1 200 OK\n");
-				char error_message[400] = "\r\r<html><body>400 Bad Request Reason: Invalid URL:";
+				char error_message[400] = "<html><body>400 Bad Request Reason: Invalid URL:";
 				strcat(error_message, pch);
 				strcat(error_message, "</body></html>\n");
-				char connection[40] = "Connection: Close\n";
+				char connection[40] = "Connection: Close\r\n\r\n";
 				char length[40] = ""; 
 				char type[40] = "Content-Type: text/html\n";
 				sprintf(length, "Content-Length: %d\n", strlen(error_message));
