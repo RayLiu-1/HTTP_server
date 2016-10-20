00f41b89f2f651732e39c3dc1435fc41ad1526d1
RayLiu-1 <ruli7792@colorado.edu>
2016-10-20 01:53:15 -0600

    test

--- Server.c
+++ Server.c
@@ -92,6 +92,7 @@ void *connection_handler(void *sockfd) {
 		//Send the message back to client
 		memset(buf, 0, BUFSIZE);
 		n = recv(cnfd, buf, BUFSIZE, 0);
+		puts(buf);
 		//pch = strtok(buf)
 		if (n == -1) {
 			puts("Time out");
