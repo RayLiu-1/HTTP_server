ceef02e74f22e28b31d9bb4494fc515f721da9bd
RayLiu-1 <ruli7792@colorado.edu>
2016-10-19 23:17:54 -0600

    test

--- Server.c
+++ Server.c
@@ -287,7 +287,7 @@ void *connection_handler(void *sockfd) {
 				}
 				strcat(sendbuf, "\r\n\r\n");
 				write(cnfd, sendbuf, strlen(sendbuf) + 1);
-				puts(sendbuf);
+				//puts(sendbuf);
 				memset(sendbuf, 0, BUFSIZE);
 				while (fgets(sendbuf, BUFSIZE, (FILE*)fp) != NULL) {
 					write(cnfd, sendbuf, strlen(sendbuf) + 1);
