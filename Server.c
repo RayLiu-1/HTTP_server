f0acb542667f8002769a149386aeebfdb38833dd
RayLiu-1 <ruli7792@colorado.edu>
2016-10-20 02:06:08 -0600

    test

--- Server.c
+++ Server.c
@@ -265,6 +265,7 @@ void *connection_handler(void *sockfd) {
 					strcat(sendbuf, connection);
 					strcat(sendbuf, error_message);
 					write(cnfd, sendbuf, strlen(sendbuf));
+					puts("duan");
 					fclose(fp);
 					continue;
 				}
