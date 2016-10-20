cea32a3df47ee7b7425587793fa57787bc5e2071
RayLiu-1 <ruli7792@colorado.edu>
2016-10-19 21:12:20 -0600

    test

--- Server.c
+++ Server.c
@@ -90,7 +90,9 @@ void *connection_handler(void *sockfd) {
 			printf("unable to set socket");
 		}*/
 		//Send the message back to client
+		memset(buf, 0, BUFSIZE);
 		n = recv(cnfd, buf, BUFSIZE, 0);
+		puts(buf);
 		//pch = strtok(buf)
 		if (n == -1) {
 			puts("Time out");
