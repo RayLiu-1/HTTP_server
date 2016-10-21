f8519a00ff5c21f36f4c8dee654e532063a5e185
RayLiu-1 <ruli7792@colorado.edu>
2016-10-20 22:57:17 -0600

    test

--- Server.c
+++ Server.c
@@ -154,7 +154,7 @@ void *connection_handler(void *sockfd) {
 				}
 			}
 			char* content = buf;			
-			content = strstr(buf, "\n");
+			content = strstr(lastbuf, "\n");
 			if (strlen(content) > 2) {
 				fwrite(content+2, 1, content-buf-2, fp);
 				recv_size -= content - buf - 2;
